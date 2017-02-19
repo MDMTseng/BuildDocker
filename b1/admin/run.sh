091761317a554103cfeb4d0fe0fb0c3ba4934aa4
MDM Tseng <chiapintseng@gmail.com>
2017-02-19 02:36:22 -0800

    Update run volume mount

--- b1/admin/run.sh
+++ b1/admin/run.sh
@@ -1,1 +1,5 @@
-docker run -d -v b1_vol:/home/ctseng/workspace  -p 2022:22 b1
+volumeName=b1_workspace
+docker volume ls|grep $volumeName
+[ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
+
+docker run -d -v $volumeName:/home/ctseng/workspace  -p 2022:22 b1

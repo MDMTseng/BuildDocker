bbd4b8298c5de2dc4ab5a411ada6b9b28c6b7834
MDM Tseng <chiapintseng@gmail.com>
2017-02-19 02:10:59 -0800

    refine the external file flow

--- b1/build.sh
+++ b1/build.sh
@@ -1,5 +1,12 @@
 #The environment should be in b1
-[ ! -d ./pybin ]&& echo "please extract pybin folder in the folder with Dockerfile" && exit 1
+[ ! -d ./external/pybin ]&& echo "please extract pybin folder in the folder with Dockerfile" && exit 1
+
+GCC_ARM_FILE=gcc-arm-none-eabi-4_7-2013q3-20130916-linux.tar.bz2
+if [ ! -f ./external/$GCC_ARM_FILE ];
+  then echo "No file $GCC_ARM_FILE found in external folder try to download it.."
+  wget https://launchpad.net/gcc-arm-embedded/4.7/4.7-2013-q3-update/+download/$GCC_ARM_FILE -P ./external/
+  [ $? -ne 0 ] && exit 1
+fi
 
 echo To build this image it needs user Name\&PW
 echo -n UserName:

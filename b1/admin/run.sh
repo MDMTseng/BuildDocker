ab4e0c21545483d2464c036644c8e06d92813d5d
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-02-21 11:19:21 -0800

    refine run script

--- b1/admin/run.sh
+++ b1/admin/run.sh
@@ -1,5 +1,9 @@
 volumeName=b1_workspace
 docker volume ls|grep $volumeName
 [ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
+username=$1
+[ -z $username ]&& echo "specify your username.."&& exit 1
+docker run -d \
+-p 2022:22 b1 \
+-v $volumeName:/home/$username/workspace \
 
-docker run -d -v $volumeName:/home/ctseng/workspace  -p 2022:22 b1

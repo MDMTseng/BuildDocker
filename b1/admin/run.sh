7590abddcf0885c1192bdfe6f44ca86df81f4e5b
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-03-14 14:34:26 -0700

    Sp setup

--- b1/admin/run.sh
+++ b1/admin/run.sh
@@ -1,9 +1,12 @@
-volumeName=b1_workspace
+volumeName=b1_vol
 docker volume ls|grep $volumeName
 [ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
 username=$1
 [ -z $username ]&& echo "specify your username.."&& exit 1
 docker run -d \
--p 2022:22 b1 \
+-p 2022:22 \
 -v $volumeName:/home/$username/workspace \
+-v b1_AUS:/home/$username/workspace_AUS \
+-v /Users/ctseng/Documents/workspace/BANZAI_X/CHOPES/:/home/$username/CHPX \
+b1
 

21381d31fd483526fe49b21eec9fe91a5d8e903d
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-04-27 15:15:48 -0700

    Update tf bash

--- tf_ssh/admin/run.sh
+++ tf_ssh/admin/run.sh
@@ -1,9 +1,11 @@
 volumeName=b1_vol
 docker volume ls|grep $volumeName
 [ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
-docker run -d \
+docker run -it \
 -v $volumeName:/workspace \
 -v /Library/WebServer/Documents/webup:/WEBUP \
 -v /Users/ctseng/workspace:/ct_ws \
---name tf_ssh tf_ssh tail -f /dev/null
+--name tf_ssh tf_ssh /bin/bash
+
+docker rm tf_ssh
 

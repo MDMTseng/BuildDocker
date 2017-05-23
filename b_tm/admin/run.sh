9244621dec1f1195713d3ee5f535298983c4ca2c
MDMTseng <chiapintseng@gmail.com>
2017-05-23 10:16:20 -0700

    no message

---
+++ b_tm/admin/run.sh
@@ -0,0 +1,15 @@
+volumeName=b1_vol
+docker volume ls|grep $volumeName
+[ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
+username=$1
+[ -z $username ]&& echo "specify your username.."&& exit 1
+docker run -d \
+-p 2022:22 \
+-p 111:111 \
+-p 2049:2049 \
+-v $volumeName:/home/$username/workspace \
+-v /Users/ctseng/Documents/workspace/BANZAI_X/CHOPES/:/home/$username/CHPX \
+-v /Library/WebServer/Documents/webup:/home/$username/WEBUP \
+-v /Volumes:/home/$username/Volumes \
+b1
+
---
+++ b_tm/build.sh
@@ -0,0 +1,17 @@
+#The environment should be in b1
+[ ! -d ./external/pybin ]&& echo "please extract pybin folder in the folder with Dockerfile" && exit 1
+
+GCC_ARM_FILE=gcc-arm-none-eabi-4_7-2013q3-20130916-linux.tar.bz2
+if [ ! -f ./external/$GCC_ARM_FILE ];
+  then echo "No file $GCC_ARM_FILE found in external folder try to download it.."
+  wget https://launchpad.net/gcc-arm-embedded/4.7/4.7-2013-q3-update/+download/$GCC_ARM_FILE -P ./external/
+  [ $? -ne 0 ] && exit 1
+fi
+
+echo To build this image it needs user Name\&PW
+echo -n UserName:
+read  username
+echo -n "Password:(won't show what you type)"
+read -s password
+docker build --build-arg user=$username --build-arg pw=$password -t b1 .
+exit $?

d4d1b8c66642cd855717a1ceebfea4267004d920
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-08-14 12:18:10 -0700

    Change b_tm run privilige

--- b_tm/admin/run.sh
+++ b_tm/admin/run.sh
@@ -3,12 +3,15 @@ docker volume ls|grep $volumeName
 [ $? -ne 0 ]&&(echo "cannot find docker volume:$volumeName. creat it"; docker volume create $volumeName)
 username=$1
 [ -z $username ]&& echo "specify your username.."&& exit 1
-docker run -d \
+docker run \
+--privileged=true \
+--cap-add=SYS_ADMIN \
+-d \
 -p 2022:22 \
 -p 111:111 \
 -p 2049:2049 \
 -v $volumeName:/home/$username/workspace \
--v /Users/ctseng/Documents/workspace/BANZAI_X/CHOPES/:/home/$username/CHPX \
+-v /Users/ctseng/Documents/workspace/BANZAI_X/:/home/$username/BZ_X \
 -v /Library/WebServer/Documents/webup:/home/$username/WEBUP \
 -v /Volumes:/home/$username/Volumes \
 b_tm
